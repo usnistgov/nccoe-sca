2ab9543500513bf57c43e9b222a9ae6e9b54e863
Chris Brown <cjbrown@mitre.org>
2021-05-06 14:50:53 -0400

    Update PXE/Eclypsium Portable Scanner Centos7.sh

--- PXE/Eclypsium Portable Scanner Centos7.sh
+++ PXE/Eclypsium Portable Scanner Centos7.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-eclypsium_script="eclypsium_agent_deployer.run"
+eclypsium_script="./eclypsium_agent_deployer.run"
 provisioner_script="hirs-provisioner"
 allcomponents_script="/opt/paccor/scripts/allcomponents.sh"
 allcomponents_output_file="system.json"
-collator_host="collator.sca.nccoe.org"
+collator_host="archer.nccoe.org"
 collator_path="/upload"
 
 # Script to scan and register computing device in the Eclypsium service from Centos7 network boot environment
@@ -13,20 +13,25 @@ collator_path="/upload"
 ProductName=`dmidecode --string='system-product-name'`
 SerialNumber=`dmidecode --string='system-serial-number'`
 
-echo "Changing hostname to $SerialNumber$ProductName"
+#echo "Changing hostname to $SerialNumber$ProductName"
 hostnamectl set-hostname "$SerialNumber$ProductName"
 
 echo "Scanning and registering client device to Eclypsium..."
-UUID=`dmidecode -s system-uuid`
-bash "$eclypsium_script -- --run -s1 demo-0124.eclypsium.cloud sec6pHdBTOFgihZ8utdlSjeDmX6n_XHqN6kQANsbNS668k -disable-cert-verification -medium -custom-id $UUID"
+#UUID=`dmidecode -s system-uuid`
+#$eclypsium_script -- --run -s1 demo-0124.eclypsium.cloud sec6pHdBTOFgihZ8utdlSjeDmX6n_XHqN6kQANsbNS668k -disable-cert-verification -medium -custom-id $UUID
 
-echo "Running the HIRS ACA provisioner..."
-bash "$provisioner_script -p"
+#echo "Running the HIRS ACA provisioner..."
+#$provisioner_script -p
 
-echo "Exporting the system and component data..."
-bash "$allcomponents_script > $allcomponents_output_file"
+# if provision succeeds ...
 
-echo "Pushing system and component data to collator..."
-curl -F "jsonFile=@$allcomponents_output_file" -F "UUID=$UUID" http://$collator_host$collator_path 
+if [ $? -eq 0 ]
+then
+        echo "Provisioning succeeded. Exporting the system and component data..."
+        $allcomponents_script /tmp > $allcomponents_output_file
 
-
+        echo "Pushing system and component data to collator..."
+        #curl -F "jsonFile=@$allcomponents_output_file" -F "UUID=$UUID" -F "type=HIRS" http://$collator_host$collator_path
+else
+        echo "Provisioning failed."
+fi
