33d3d15133b8cec73313e181440436e47cad5163
Chris Brown <cjbrown@mitre.org>
2021-08-23 09:39:50 -0400

    Update PXE/Eclypsium Portable Scanner Centos7.sh

--- PXE/Eclypsium Portable Scanner Centos7.sh
+++ PXE/Eclypsium Portable Scanner Centos7.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 eclypsium_script="./eclypsium_agent_deployer.run"
+eclypsium_build_script="./eclypsium.run"
 provisioner_script="hirs-provisioner"
 allcomponents_script="/opt/paccor/scripts/allcomponents.sh"
 allcomponents_output_file="system.json"
-collator_host="archer.nccoe.org"
+collator_host="collator.lab.nccoe.org"
 collator_path="/upload"
 
 # Script to scan and register computing device in the Eclypsium service from Centos7 network boot environment
@@ -12,26 +13,32 @@ collator_path="/upload"
 # Change the hostname to something more helpful, while in the Eclypsium dashboard
 ProductName=`dmidecode --string='system-product-name'`
 SerialNumber=`dmidecode --string='system-serial-number'`
+VMWARE="VMware Virtual Platform"
 
-#echo "Changing hostname to $SerialNumber$ProductName"
+echo "Changing hostname to $SerialNumber$ProductName"
 hostnamectl set-hostname "$SerialNumber$ProductName"
 
-echo "Scanning and registering client device to Eclypsium..."
-#UUID=`dmidecode -s system-uuid`
-#$eclypsium_script -- --run -s1 demo-0124.eclypsium.cloud sec6pHdBTOFgihZ8utdlSjeDmX6n_XHqN6kQANsbNS668k -disable-cert-verification -medium -custom-id $UUID
-
-#echo "Running the HIRS ACA provisioner..."
-#$provisioner_script -p
-
-# if provision succeeds ...
-
-if [ $? -eq 0 ]
-then
-        echo "Provisioning succeeded. Exporting the system and component data..."
-        $allcomponents_script /tmp > $allcomponents_output_file
-
-        echo "Pushing system and component data to collator..."
-        #curl -F "jsonFile=@$allcomponents_output_file" -F "UUID=$UUID" -F "type=HIRS" http://$collator_host$collator_path
+UUID=`dmidecode -s system-uuid`
+if [ "$ProductName" != "$VMWARE" ]; then
+	echo "Building the Eclypsium scanner..."
+	$eclypsium_build_script
+	echo "Scanning and registering client device to Eclypsium..."
+	$eclypsium_script -- --run -s1 demo-0124.eclypsium.cloud secxvhI545Es4gj1eud1S4c38UI2NBkmjFmEylx4HLnYFE -ca-cert-path /etc/pki/tls/certs/ca-bundle.crt -medium -custom-id $UUID
 else
-        echo "Provisioning failed."
+	echo "VM detected, skipping Eclypsium registration"
 fi
+
+echo "Running the HIRS ACA provisioner..."
+$provisioner_script -p
+
+# exit script if provisioning was not successful
+if [ $? -ne 0 ]; then
+	echo "HIRS ACA provisioning failed. Exiting script"
+	exit 1
+fi
+
+echo "Exporting the system and component data..."
+$allcomponents_script > $allcomponents_output_file
+
+echo "Pushing system and component data to collator..."
+curl -F "jsonFile=@$allcomponents_output_file" -F "UUID=$UUID" -F "type=HIRS" http://$collator_host$collator_path 
